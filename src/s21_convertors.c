52828d518fa0a2fca2914ffc614d84182b2a4a99
Darina Ayurova <comedial@student.21-school.ru>
2023-01-12 21:37:22 +0300

    добавила зануление децимал в флоат ту децимал

--- src/s21_convertors.c
+++ src/s21_convertors.c
@@ -2,6 +2,7 @@
 
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
   int resCode = 0, f_scale = 0;
+  memset(dst, 0, sizeof(*dst));
   if (src != NAN && src != INFINITY && src != 0) {
     if (src < 0) {
       src *= -1;
@@ -9,8 +10,7 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
     } else
       setSign(dst, 0);
     int binExp = float_getScale(src);
-    if (binExp > 95)
-      resCode = 1;
+    if (binExp > 95) resCode = 1;
     if (src < 1e8 && !resCode) {
       for (; !((int)src); src *= 10, f_scale++) {
       }
@@ -20,15 +20,13 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
       }
       binExp = float_getScale(src);
     }
-    if (f_scale > 28 || binExp < -94)
-      resCode = 1;
+    if (f_scale > 28 || binExp < -94) resCode = 1;
     if (!resCode) {
       setBit(dst, binExp, 1);
       unsigned int fbits = *((unsigned int *)&src);
       for (unsigned int mask = 0x400000; mask; mask >>= 1) {
         binExp--;
-        if (!!(fbits & mask))
-          setBit(dst, binExp, 1);
+        if (!!(fbits & mask)) setBit(dst, binExp, 1);
       }
       setScale(dst, f_scale);
     }
@@ -41,12 +39,10 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
   int resCode = 0;
   *dst = 0;
   for (int i = 0; i < 96; i++) {
-    if (getBit(src, i))
-      *dst += pow(2, i);
+    if (getBit(src, i)) *dst += pow(2, i);
   }
   *dst *= pow(10, (-1 * (getScale(src))));
-  if (getSign(src))
-    *dst *= -1;
+  if (getSign(src)) *dst *= -1;
   return resCode;
 }
 
@@ -70,12 +66,10 @@ int s21_from_decimal_to_int(s21_decimal src, int *dst) {
   else {
     *dst = 0;
     for (int i = 0; i < 96; i++) {
-      if (getBit(src, i))
-        *dst += pow(2, i);
+      if (getBit(src, i)) *dst += pow(2, i);
     }
     *dst *= pow(10, (getScale(src) * -1));
-    if (getSign(src))
-      *dst *= -1;
+    if (getSign(src)) *dst *= -1;
   }
   return resCode;
 }
